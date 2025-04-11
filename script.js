a75797e26dca5e231d5622ae564dd8adf61506e1
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-04-11 12:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById('sum_btn').addEvenListener('click', function() {
+	const num1 = Number(document.getElementById('n1').value);
+	const num1 = Number(document.getElementById('n2').value);
+
+	if(!isNan(num1) && !isNan(num2)){
+		const result = num1 + num2;
+		document.getElementById('sum').textContent = `Sum: ${result}`;
+	}
+	else{
+		document.getElementById('sum').textContent = '';
+	}
+});
